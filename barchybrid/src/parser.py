b81da0bc12a9c63a85cab503f965a8dcfb9ffd34
miryam <mdelhoneux@gmail.com>
2017-04-06 16:20:09 +0200

    s/tespath/testpath

--- barchybrid/src/parser.py
+++ barchybrid/src/parser.py
@@ -73,16 +73,16 @@ if __name__ == '__main__':
         parser = ArcHybridLSTM(words, pos, rels, w2i, stored_opt)
         parser.Load(options.model)
         conllu = (os.path.splitext(options.conll_test.lower())[1] == '.conllu')
-        tespath = os.path.join(options.output, 'test_pred.conll' if not conllu else 'test_pred.conllu')
+        testpath = os.path.join(options.output, 'test_pred.conll' if not conllu else 'test_pred.conllu')
         ts = time.time()
         pred = list(parser.Predict(options.conll_test))
         te = time.time()
-        utils.write_conll(tespath, pred)
+        utils.write_conll(testpath, pred)
 
         if not conllu:
-            os.system('perl src/utils/eval.pl -g ' + options.conll_test + ' -s ' + tespath  + ' > ' + tespath + '.txt')
+            os.system('perl src/utils/eval.pl -g ' + options.conll_test + ' -s ' + testpath  + ' > ' + testpath + '.txt')
         else:
-            os.system('python src/utils/evaluation_script/conll17_ud_eval.py -v -w src/utils/evaluation_script/weights.clas ' + options.conll_test + ' ' + tespath + ' > ' + testpath + '.txt')
+            os.system('python src/utils/evaluation_script/conll17_ud_eval.py -v -w src/utils/evaluation_script/weights.clas ' + options.conll_test + ' ' + testpath + ' > ' + testpath + '.txt')
         
         print 'Finished predicting test',te-ts
 
